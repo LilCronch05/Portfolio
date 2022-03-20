da117517e3d3ccc28353275a9f41af69e6da4cfb
LilCronch05 <abibault@gmail.com>
2022-03-20 00:30:07 -0600

    progress #2

--- app.js
+++ app.js
@@ -4,19 +4,34 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 
-function pageTransitions(){
+function PageTransitions(){
     //Button click active class
     for(let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn';
+            this.className += ' active-btn';
         })
     }
     
     //Sectinons active class
     allSections.addEventListener('click', (e) =>{
-        console.log(e.target);
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((sections) =>{
+                sections.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
     })
 }
 
